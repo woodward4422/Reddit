0fdbdfa38507d5253843343d0dfed71dc655e888
noah <noahwoodward2244@gmail.com>
2019-01-01 21:22:44 -0500

    Basic Project Setup and one Route

---
+++ controllers/posts.js
@@ -0,0 +1,13 @@
+const Post = require('../models/post');
+
+module.exports = (app) => {
+
+    app.post('/posts/new', (req, res) => {
+      const post = new Post(req.body);
+      post.save((err, post) => {
+        return res.redirect(`/`);
+      })
+    });
+  
+  };
+  
---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express')
+var exphbs = require('express-handlebars');
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
+
+const app = express()
+
+require('./controllers/posts.js')(app);
+require('./data/reddit-db');
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(expressValidator());
+
+
+app.get('/', (req, res) => {
+  res.render('home')
+})
+
+app.get('/posts/new', (req, res) => {
+    res.render('posts-new')
+  })
+
+app.listen(3000, () => {
+  console.log('App listening on port 3000!')
+})
